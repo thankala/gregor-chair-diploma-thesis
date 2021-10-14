8b6131fccde7ed1d1c3c7387c1a153453767658d
Thanasis Kalatheris <kalatheristhanasis@gmail.com>
2021-10-14 21:36:37 +0300

    Fixed typo in files name

--- robot_1/robot_1_controller/services/screw_pick_and_fasten.py
+++ robot_1/robot_1_controller/services/screw_pick_and_fasten.py
@@ -2,13 +2,13 @@ import time
 
 
 class ScrewPickAndFasten:
-    COMPLETED_MESSAGE = '{"ScrewPickAndFasten": "finished"}'
 
     def __init__(self):
-        print("\n \n ScrewPickAndFasten service started \n \n")
+        print("\n \n ScrewPickAndFasten Service started \n \n")
 
     def start_working(self, device):
         try:
+            print('ScrewPickAndFasten Started')
             from gpio.gpio_controller import GpioController
             gpio = GpioController()
             gpio.init_pins()
@@ -20,9 +20,7 @@ class ScrewPickAndFasten:
             time.sleep(0.5)
             gpio.gpio_control('led', 1)
             time.sleep(0.5)
-            print('ScrewPickAndFasten finished..')
-            print('Replying to WT server...')
-            encoded_response = self.COMPLETED_MESSAGE
-            return encoded_response
+            print('ScrewPickAndFasten Finished')
+            return
         except (ConnectionResetError, OSError) as e:
             print(e)
